f20b94724e7e034327f75abf790733166f608cd2
Nikita <nikitabushhan@gmail.com>
2023-01-08 20:37:19 +0200

    add project

---
+++ Task/Client/Program.cs
@@ -0,0 +1,193 @@
+﻿using System.Net.Sockets;
+
+// Client
+
+string filePath = "nums.dat";
+
+int port = 4002;
+
+UdpClient client = new UdpClient(port);
+
+long gotPackages = 0;
+
+var possibleToWrite = true;
+var fileClosed = false;
+
+var tempArray = new List<int>();
+
+Task.Run(ReceiveMessageAsync).ConfigureAwait(false).GetAwaiter();
+
+await Task.Run(ConsoleController);
+
+async Task ReceiveMessageAsync()
+{
+    var file = File.Open(filePath, FileMode.OpenOrCreate);
+    var binaryWriter = new BinaryWriter(file);
+
+    while (true)
+    {
+        var result = await client.ReceiveAsync();
+
+        // getting data on the number of sent packages
+        if (result.Buffer.Length == 8)
+        {
+            Console.WriteLine($"gett: {BitConverter.ToUInt64(result.Buffer, 0)}");
+
+            gotPackages = BitConverter.ToInt64(result.Buffer, 0);
+
+            continue;
+        }
+
+        var receiveNum = BitConverter.ToInt32(result.Buffer, 0);
+
+        if (possibleToWrite == false)
+        {
+            if (fileClosed == false)
+            {
+                // close connection with file
+                file.Close();
+                binaryWriter.Close();
+
+                fileClosed = true;
+            }
+
+            // save to temp array
+            tempArray.Add(receiveNum);
+            continue;
+        }
+
+        if (tempArray.Count > 0)
+        {
+            // connection to file
+            file = File.Open(filePath, FileMode.OpenOrCreate);
+            binaryWriter = new BinaryWriter(file);
+
+            // write all data from temp array to file
+            foreach (var num in tempArray)
+            {
+                binaryWriter.Write(num);
+            }
+
+            tempArray = new List<int>();
+        }
+
+        // write data to file
+        binaryWriter.Write(receiveNum);
+    }
+
+    file.Close();
+    binaryWriter.Close();
+}
+
+Task ConsoleController()
+{
+    while (true)
+    {
+        var key = Console.ReadKey();
+
+        if (key.Key == ConsoleKey.Enter)
+        {
+            var start = DateTime.UtcNow;
+
+            // block write to file
+            possibleToWrite = false;
+
+            var packs = gotPackages;
+
+            while (true)
+            {
+                if (fileClosed)
+                {
+                    var file = File.Open(filePath, FileMode.Open);
+                    var binaryReader = new BinaryReader(file);
+
+                    long sum = 0;
+                    long numsCount = 0;
+
+                    Dictionary<int, ulong> numsMode = new Dictionary<int, ulong>();
+
+                    // read data from file
+                    while (binaryReader.PeekChar() > -1)
+                    {
+                        int num = binaryReader.ReadInt32();
+
+                        // building dictionary with numbers and count of repeats this num
+                        if (numsMode.TryAdd(num, 1) == false)
+                        {
+                            numsMode[num]++;
+                        }
+
+                        sum += num;
+                        numsCount++;
+                    }
+
+                    // find median
+                    var medianFileCursorPostion = (numsCount / 2 - 1) * sizeof(int);
+
+                    file.Position = medianFileCursorPostion;
+
+                    var median = binaryReader.ReadInt32();
+
+                    // getting average value
+                    var average = sum / numsCount;
+
+                    int modeNumber = 0;
+                    ulong modeCount = 0;
+
+                    double squareSum = 0;
+
+                    // prepair data to calc standard deviation and find mode
+                    Parallel.ForEach(numsMode, num =>
+                    {
+                        var diff = num.Value - (ulong)average;
+
+                        var square = Math.Pow(diff, 2);
+
+                        squareSum += square;
+
+                        // find mode
+                        if (num.Value > modeCount)
+                        {
+                            modeNumber = num.Key;
+                            modeCount = num.Value;
+                        }
+                    });
+
+                    // calc standard deviation
+                    ulong zeros = 0;
+
+                    if (numsMode.TryGetValue(0, out ulong value) == true)
+                    {
+                        zeros = numsMode[0];
+                    }
+
+                    var res = squareSum / (numsCount - (long)zeros) - 1;
+
+                    var deviation = Math.Sqrt(res);
+
+                    Console.WriteLine($"Count: {numsCount}");
+                    Console.WriteLine($"Average: {average}");
+                    Console.WriteLine($"Packs: {packs}");
+                    Console.WriteLine($"Loss: {packs - numsCount}");
+                    Console.WriteLine($"Mod: {modeNumber}");
+                    Console.WriteLine($"Deviation: {deviation}");
+                    Console.WriteLine($"Median: {median}");
+                    Console.WriteLine();
+
+                    file.Close();
+                    binaryReader.Close();
+
+                    possibleToWrite = true;
+                    fileClosed = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"calc time: {DateTime.UtcNow - start}\n\n");
+        }
+        else if (key.Key == ConsoleKey.Escape)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Task/Server/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Xml.Linq;
+using System.Net.Sockets;
+
+// Server
+
+XDocument xmlConfig;
+
+int fromRange, toRange, tasksCount;
+
+try
+{
+    xmlConfig = XDocument.Load("config.xml");
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message + '\n');
+    throw;
+}
+
+XElement? serverConfig = xmlConfig.Element("server");
+
+if (serverConfig == null)
+{
+    Console.WriteLine("server object in config not found");
+    return;
+}
+
+XElement? range = serverConfig.Element("range");
+
+if (range == null)
+{
+    Console.WriteLine("range object in config is not found");
+    return;
+}
+
+#region WithCheckingCorrectData
+
+//// from Range
+
+//XElement? fromRangeElement = range.Element("from");
+
+//if (fromRangeElement == null)
+//{
+//    Console.WriteLine("from Range value in config is not found");
+//    return;
+//}
+
+//if (int.TryParse(fromRangeElement.Value, out fromRange) == false)
+//{
+//    Console.WriteLine("fromRangeElement has wrong data type");
+//    return;
+//}
+
+//// to Range
+
+//XElement? toRangeElement = range.Element("to");
+
+//if (toRangeElement == null)
+//{
+//    Console.WriteLine("to Range value in config is not found");
+//    return;
+//}
+
+//if (int.TryParse(toRangeElement.Value, out toRange) == false)
+//{
+//    Console.WriteLine("toRangeElement has wrong data type");
+//    return;
+//}
+
+//// Get tasks count
+
+//XElement? tasksCountElement = serverConfig.Element("tasks");
+
+//if (tasksCountElement == null)
+//{
+//    Console.WriteLine("tasks value in config is not found");
+//    return;
+//}
+
+//if (int.TryParse(tasksCountElement.Value, out tasksCount) == false)
+//{
+//    Console.WriteLine("tasksCountElement has wrong data type");
+//    return;
+//}
+
+#endregion
+
+#region WithoutChecking
+
+tasksCount = Convert.ToInt32(serverConfig.Element("tasks")?.Value);
+fromRange = Convert.ToInt32(range.Element("from")?.Value);
+toRange = Convert.ToInt32(range.Element("to")?.Value);
+
+#endregion
+
+int port = 4002;
+string ipAddress = "127.0.0.1";
+
+ulong packages = 0;
+
+UdpClient server = new UdpClient(ipAddress, port);
+
+var tasks = new List<Task>();
+
+for (int i = 0; i < tasksCount; i++)
+{
+    tasks.Add(Task.Run(() => SendNumberAsync(fromRange, toRange)));
+}
+
+tasks.Add(Task.Run(async () =>
+{
+    while (true)
+    {
+        await Task.Delay(1000);
+
+        byte[] data = BitConverter.GetBytes(packages);
+
+        await server.SendAsync(data);
+    }
+}));
+
+Console.ReadLine();
+
+async Task SendNumberAsync(int fromRange, int toRange)
+{
+    while (true)
+    {
+        byte[] data = BitConverter.GetBytes(new Random().Next(fromRange, toRange));
+
+        await server.SendAsync(data);
+
+        packages++;
+    }
+}
